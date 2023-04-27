588204aa9794194d8c6c109c97aa19c6d3b1ad34
Piotrek <piotrek.piotr2@gmail.com>
2023-04-27 19:58:34 +0200

    init

---
+++ src/test/java/com/rojek/dietplanner/DietplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rojek.dietplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
